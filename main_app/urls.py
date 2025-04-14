46d5bf3ec469476770ebf302dbc5b82c314a1c85
msttsm831 <Mohdthamer02@gmail.com>
2025-04-14 20:48:00 +0300

    Rendering + Home

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+      # route for cats index
+  path('cats/', views.cat_index, name='cat-index'),
+
 ]
