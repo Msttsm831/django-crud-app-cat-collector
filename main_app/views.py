2bc97290205f53ba21aaa17e6758c10e9e26efd3
msttsm831 <Mohdthamer02@gmail.com>
2025-04-20 20:41:39 +0300

    added edit and delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+from django.shortcuts import render, redirect
+
 # Create your views here.
 
 # views.py
@@ -33,4 +38,34 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # include the cat and feeding_form in the context
+        'cat': cat, 'feeding_form': feeding_form
+    })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
